a69095791aab235b6451a51a163528921a1345eb
Maryna-Danyliuk <106257899+Maryna-Danyliuk@users.noreply.github.com>
2024-01-25 00:46:59 +0200

    done task 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const registerForm = document.querySelector('.login-form');
+const btnLogin = document.querySelector('button')
+console.log(btnLogin);
+btnLogin.addEventListener('click', onBtnSubmit );
+// function onBtnSubmit (){
+//     console.log('Hello BaBy!')
+// }
+function onBtnSubmit (event){
+event.preventDefault();
 
+
+const {email, password} = event.currentTarget.elements;
+if(!email.value.trim() || !password.value.trim()){
+return alert('All form fields must be filled in')
+}
+else{
+    const accountsObj = {
+        email: email.value,
+        password: password.value,
+    };
+    console.log(email.value);
+    event.currentTarget.reset();
+ }
+
+}
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color')
+btnChangeColor.addEventListener('click', onButtonClick);
+function onButtonClick(){
+  let color = getRandomHexColor();
+  body.style.background = color;
+  btnChangeColor.style.background = color;
+}
